7b3a54e289b7b249150ebf83bbf84517a6f96fc7
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-18 17:38:32 +0430

    [nats] feat: connect to server

--- nats/client/index.js
+++ nats/client/index.js
@@ -1,14 +1,30 @@
+import { connect } from "nats";
+
 class Client {
     constructor() {
-
+        this.servers = [
+            {},
+            { servers: ["demo.nats.io:4442", "demo.nats.io:4222"] },
+            { servers: "demo.nats.io:4443" },
+            { port: 4222 },
+            { servers: "localhost" },
+        ];
     }
 
     connect() {
+        this.servers.forEach(async(v) => {
+            try {
+                const nc = await connect(v);
 
-    }
+                console.log(`connected to ${nc.getServer()}`);
 
-    disconnect() {
+                return nc;
+            } catch (err) {
+                console.log(`error connecting to ${JSON.stringify(v)}`);
 
+                return null;
+            }
+        });
     }
 }
 
