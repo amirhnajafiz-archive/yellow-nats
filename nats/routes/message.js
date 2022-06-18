60cff310621d75101c216a6f40ce2992c662f38b
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-18 10:27:59 +0430

    [nats] add: message handler

--- nats/routes/message.js
+++ nats/routes/message.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+/* TODO: publish a message on nats */
+router.post('/publish', function (_req, res, _next) {
+    res.json(
+        {
+            'message': 'OK'
+        }
+    );
+})
+
+/* TODO: subscribe on a message */
+router.get('/subscribe', function (_req, res, _next) {
+
+})
 
 module.exports = router;
