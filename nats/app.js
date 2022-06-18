66f52ad80fbeebff5f71c49b1ad329b9eb111c82
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-18 10:20:17 +0430

    [nats] add: message router

--- nats/app.js
+++ nats/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const messageRouter = require('./routes/message');
 
 let app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(_req, _res, next) {
