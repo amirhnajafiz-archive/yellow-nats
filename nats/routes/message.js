48772f30d1423982d9b9c08a9db6ef025e3fb755
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-18 10:46:40 +0430

    [nats] feat: client in message route

--- nats/routes/message.js
+++ nats/routes/message.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const cli = require('../client');
 
 /* TODO: publish a message on nats */
 router.post('/publish', function (_req, res, _next) {
+    cli.prototype.connect();
+
     res.json(
         {
             'message': 'OK'
         }
     );
+
+    cli.prototype.disconnect();
 })
 
 /* TODO: subscribe on a message */
 router.get('/subscribe', function (_req, res, _next) {
-
+    cli.prototype.connect();
+    cli.prototype.disconnect();
 })
 
 module.exports = router;
