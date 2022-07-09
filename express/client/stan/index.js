e3bc9a3569652d5523a7a7c758d23e764392f30c
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-09 09:39:11 +0430

    feat: start method for stan

--- express/client/stan/index.js
+++ express/client/stan/index.js
@@ -21,34 +21,43 @@ class Client {
             return null;
         }
     }
-}
 
-sc.on('connect', () => {
-    // Simple Publisher (all publishes are async in the node version of the client)
-    sc.publish('foo', 'Hello node-nats-streaming!', (err, guid) => {
-        if (err) {
-            console.log('publish failed: ' + err)
-        } else {
-            console.log('published message with guid: ' + guid)
+    start() {
+        const sc = this.connect()
+        if (sc === null) {
+            console.error("Stan connection failed")
+
+            return
         }
-    })
 
-    // Subscriber can specify how many existing messages to get.
-    const opts = sc.subscriptionOptions().setStartWithLastReceived()
-    const subscription = sc.subscribe('foo', opts)
-    subscription.on('message', (msg) => {
-        console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData())
-    })
+        sc.on('connect', () => {
+            // Simple Publisher (all publishes are async in the node version of the client)
+            sc.publish('foo', 'Hello node-nats-streaming!', (err, guid) => {
+                if (err) {
+                    console.log('publish failed: ' + err)
+                } else {
+                    console.log('published message with guid: ' + guid)
+                }
+            })
 
-    // After one second, unsubscribe, when that is done, close the connection
-    setTimeout(() => {
-        subscription.unsubscribe()
-        subscription.on('unsubscribed', () => {
-            sc.close()
+            // Subscriber can specify how many existing messages to get.
+            const opts = sc.subscriptionOptions().setStartWithLastReceived()
+            const subscription = sc.subscribe('foo', opts)
+            subscription.on('message', (msg) => {
+                console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData())
+            })
+
+            // After one second, unsubscribe, when that is done, close the connection
+            setTimeout(() => {
+                subscription.unsubscribe()
+                subscription.on('unsubscribed', () => {
+                    sc.close()
+                })
+            }, 1000)
         })
-    }, 1000)
-})
 
-sc.on('close', () => {
-    console.log("[STAN] Connection closed")
-})
+        sc.on('close', () => {
+            console.log("[STAN] Connection closed")
+        })
+    }
+}
