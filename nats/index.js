e24b2a45e731a740f6fe99e91e127cbabf8875e3
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-11 12:38:29 +0430

    update: methods

--- nats/index.js
+++ nats/index.js
@@ -2,26 +2,32 @@
 const nats = require('nats')
 const sc = require('nats').StringCodec()
 
-// setting the servers
+// setting the nats servers information
 cluster = [
     {servers: 'nats://localhost:4222'}
 ];
 
+// connect to nats server
 async function connect() {
     try {
+        // calling the nats connect method with
+        // clusters information
         const nc = await nats.connect(cluster);
 
+        // get the connected servers
         console.log(`connected to ${ nc.getServer() }`);
 
         return nc;
     } catch (err) {
-        console.log(`error connecting to ${JSON.stringify(this.server)}`);
+        // if an error occurs during the nats connecting
+        console.error(`error connecting to ${JSON.stringify(this.server)}`);
         console.error(err)
 
         return null;
     }
 }
 
+// subscribe handler for getting the messages
 async function handler(sub) {
     for await (const m of sub) {
         console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
@@ -30,9 +36,10 @@ async function handler(sub) {
     console.log("subscription closed");
 }
 
+// trying to test our nats server
 async function test() {
+    // first we connect to nats server
     const nc = await connect()
-    const sc = require('nats').StringCodec()
 
     if (nc === null) {
         console.error("Nats connection failed")
@@ -40,14 +47,16 @@ async function test() {
         return
     }
 
+    // subscribing on a topic
     const sub = nc.subscribe("hello");
+
+    // setting the handler
     handler(sub).then(r => console.log(r))
 
+    // publish on that topic
     nc.publish("hello", sc.encode("world"));
     nc.publish("hello", sc.encode("again"));
-
-    return "[OK] test";
 }
 
-// testing our method
+// begin testing
 test().then(r => console.log(r))
