65b13439bb4f30c1c9f702646ee877eab54770ed
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-11 12:34:52 +0430

    change: index structure

--- nats/index.js
+++ nats/index.js
@@ -1,52 +1,53 @@
+// requiring nats
 const nats = require('nats')
+const sc = require('nats').StringCodec()
 
-class Client {
-    constructor() {
-        this.server = [
-            {servers: 'nats://localhost:4222'}
-        ];
-    }
+// setting the servers
+cluster = [
+    {servers: 'nats://localhost:4222'}
+];
 
-    async connect() {
-        try {
-            const nc = await nats.connect(this.server);
+async function connect() {
+    try {
+        const nc = await nats.connect(cluster);
 
-            console.log(`connected to ${ nc.getServer() }`);
+        console.log(`connected to ${ nc.getServer() }`);
 
-            return nc;
-        } catch (err) {
-            console.log(`error connecting to ${JSON.stringify(this.server)}`);
-            console.error(err)
+        return nc;
+    } catch (err) {
+        console.log(`error connecting to ${JSON.stringify(this.server)}`);
+        console.error(err)
 
-            return null;
-        }
-    }
-
-    async start() {
-        const nc = await this.connect()
-        const sc = require('nats').StringCodec()
-
-        if (nc === null) {
-            console.error("Nats connection failed")
-
-            return
-        }
-
-        const sub = nc.subscribe("hello");
-        (async () => {
-            for await (const m of sub) {
-                console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
-            }
-            console.log("subscription closed");
-        })().then(r => console.log(r));
-
-        nc.publish("hello", sc.encode("world"));
-        nc.publish("hello", sc.encode("again"));
-
-        return "[OK] test";
+        return null;
     }
 }
 
-// creating a new client and go
-c = new Client()
-c.start().then(r => console.log(r))
+async function handler(sub) {
+    for await (const m of sub) {
+        console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+    }
+
+    console.log("subscription closed");
+}
+
+async function test() {
+    const nc = await connect()
+    const sc = require('nats').StringCodec()
+
+    if (nc === null) {
+        console.error("Nats connection failed")
+
+        return
+    }
+
+    const sub = nc.subscribe("hello");
+    handler(sub).then(r => console.log(r))
+
+    nc.publish("hello", sc.encode("world"));
+    nc.publish("hello", sc.encode("again"));
+
+    return "[OK] test";
+}
+
+// testing our method
+test().then(r => console.log(r))
