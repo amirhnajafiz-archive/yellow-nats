b0e46857b11efd6f8e2c66216bc0ae5010c4b4cd
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 09:33:06 +0430

    refactor: stan

--- stan-test/index.js
+++ stan-test/index.js
@@ -1,38 +1,38 @@
 // importing node nats streaming
-const STAN = require('node-nats-streaming')
+const STAN = require('node-nats-streaming');
 
 // cluster ID and client ID
-const CLUSTER_ID = ""
-const CLIENT_ID = ""
+const CLUSTER_ID = "";
+const CLIENT_ID = "";
 
 // topic
-const topic = "event"
+const topic = "event";
 
 // opening a connection
-const sc = STAN.connect(CLUSTER_ID, CLIENT_ID)
+const sc = STAN.connect(CLUSTER_ID, CLIENT_ID);
 
 // after connection publish on a topic
 sc.on('connect', () => {
     // Simple Publisher (all publishes are async in the node version of the client)
     sc.publish(topic, 'Hello node-nats-streaming!', (err, guid) => {
         if (err) {
-            console.log('publish failed: ' + err)
+            console.log('publish failed: ' + err);
         } else {
-            console.log('published message with guid: ' + guid)
+            console.log('published message with guid: ' + guid);
         }
     })
 })
 
 // Subscriber can specify how many existing messages to get
-const opts = sc.subscriptionOptions().setStartWithLastReceived()
-const subscription = sc.subscribe(topic, opts)
+const opts = sc.subscriptionOptions().setStartWithLastReceived();
+const subscription = sc.subscribe(topic, opts);
 
 // handler
 subscription.on('message', (msg) => {
-    console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData())
-})
+    console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData());
+});
 
 // closing connection
 sc.on('close', () => {
-    process.exit()
-})
+    process.exit();
+});
