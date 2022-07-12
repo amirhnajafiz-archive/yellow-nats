227cc82cfb5a4bd3e4a0a394bedce4b0d936f74c
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-12 09:32:30 +0430

    add: stan test

--- stan-test/index.js
+++ stan-test/index.js
@@ -5,13 +5,16 @@ const STAN = require('node-nats-streaming')
 const CLUSTER_ID = ""
 const CLIENT_ID = ""
 
+// topic
+const topic = "event"
+
 // opening a connection
 const sc = STAN.connect(CLUSTER_ID, CLIENT_ID)
 
 // after connection publish on a topic
 sc.on('connect', () => {
     // Simple Publisher (all publishes are async in the node version of the client)
-    sc.publish('foo', 'Hello node-nats-streaming!', (err, guid) => {
+    sc.publish(topic, 'Hello node-nats-streaming!', (err, guid) => {
         if (err) {
             console.log('publish failed: ' + err)
         } else {
@@ -20,10 +23,11 @@ sc.on('connect', () => {
     })
 })
 
-// Subscriber can specify how many existing messages to get.
+// Subscriber can specify how many existing messages to get
 const opts = sc.subscriptionOptions().setStartWithLastReceived()
-const subscription = sc.subscribe('foo', opts)
+const subscription = sc.subscribe(topic, opts)
 
+// handler
 subscription.on('message', (msg) => {
     console.log('Received a message [' + msg.getSequence() + '] ' + msg.getData())
 })
